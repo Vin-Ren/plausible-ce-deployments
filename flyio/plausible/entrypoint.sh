689dd34916a83812d1ce144fde8ba383aa1d0222
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2025-05-04 22:45:15 +0700

    feat: Updated entrypoint to quicken coldstart

--- flyio/plausible/entrypoint.sh
+++ flyio/plausible/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 ulimit -n 65535
 
-echo CREATING DB...
-/entrypoint.sh db createdb
-echo MIGRATING...
-/entrypoint.sh db migrate
-echo RUNNING...
-exec /entrypoint.sh run
+if [ "$1" = "release" ]; then
+  echo "Preparing plausible for release..."
+  echo CREATING DB...
+  /entrypoint.sh db createdb
+  echo MIGRATING...
+  /entrypoint.sh db migrate
+else
+  echo RUNNING PLAUSIBLE...
+  exec /entrypoint.sh run
+fi
